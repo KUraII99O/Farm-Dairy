beb8a1a8f5672069c414c0562dfe196f7c84aac4
chpeet <bommbala8@gmail.com>
2024-09-22 19:11:25 +0100

    fixed login url

--- src/component/LogIn/index.tsx
+++ src/component/LogIn/index.tsx
@@ -25,7 +25,7 @@ const LogIn: React.FC<LogInProps> = ({ onLogin }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://https://auth-api-woad.vercel.app/api/login", {
+      const response = await fetch("https://auth-api-woad.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
