59cd9792723a8b66b21d4e6330e20068ff4e3e2d
chpeet <bommbala8@gmail.com>
2024-09-26 23:18:25 +0100

    foxed some issues

--- src/component/LandingNav/index.tsx
+++ src/component/LandingNav/index.tsx
@@ -63,10 +63,10 @@ const LandingNav: React.FC<LandingNavProps> = ({
           >
             <img
               src="https://sakosys.com/envato/dairy-farm-management-system/storage/app/public/uploads/751280420015239.png"
-              className="h-8 w-auto sm:h-12 sm:w-auto ml-2 sm:ml-24"
+              className="h-6 w-auto sm:h-12 sm:w-auto ml-2 sm:ml-24"
               alt="Your Company"
             />
-            <h3 className="text-lg sm:text-xl font-bold ml-2 text-primary whitespace-nowrap">
+            <h3 className="text-lg sm:text-xl font-bold ml-2 text-primary whitespace-nowrap ml-6">
               {translate("Ges Cow")}
             </h3>
           </Link>
